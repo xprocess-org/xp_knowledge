79aa33b049f27500c83bad603e63df3522b42030
Mohamed Farahat <info@xprocess.org>
2020-01-30 13:41:34 +0200

    remove template feature to add it to doc types

--- __manifest__.py
+++ __manifest__.py
@@ -28,7 +28,6 @@
         'views/knowledge_change_request_views.xml',
         'views/knowledge_distribution_list_views.xml',
         'views/knowledge_document_type_views.xml',
-        'views/knowledge_document_template_views.xml',
         'data/knowledge.document.type.csv',
     ],
     # only loaded in demonstration mode
