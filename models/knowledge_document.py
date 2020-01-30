47129fbe1324bef418eca645f4d2140544af98d7
Mohamed Farahat <info@xprocess.org>
2020-01-30 07:43:18 +0200

    add related document and prevent self relations

--- models/knowledge_document.py
+++ models/knowledge_document.py
@@ -6,6 +6,11 @@ from odoo import _, api, fields, models
 class Document(models.Model):
     _name = 'knowledge.document'
     _description = 'Document'
+    _sql_constraints = [
+        ('knowledge_document_name_package_uq',  # Constraint unique identifier
+         'UNIQUE (name, package_id)',  # Constraint SQL syntax
+         'Document title must be unique in each package.'),  # Message
+    ]
 
     name = fields.Char('Title', required=True)
     content = fields.Html(string='Content')
@@ -30,3 +35,10 @@ class Document(models.Model):
         'knowledge.distribution.list', string='Distribution lists')
     document_type_id = fields.Many2one(
         'knowledge.document.type', string='Document type', required=True)
+    external_document = fields.Binary(string='External Document')
+    related_documents = fields.Many2many(
+        'knowledge.document',      # related model (required)
+        'knowledge_document_knowledge_document_rel',  # relation table name to use
+        'id1',             # rel table field for "this" record
+        'id2',             # rel table field for "other" record
+        'Related Documents')          # string label text
