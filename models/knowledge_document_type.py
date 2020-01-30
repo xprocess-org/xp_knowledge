645e191a84f35e1de5a6c290f31375227ad1d5bb
Mohamed Farahat <info@xprocess.org>
2020-01-30 09:13:22 +0200

    add document properties to the type

--- models/knowledge_document_type.py
+++ models/knowledge_document_type.py
@@ -9,3 +9,10 @@ class DocumentType(models.Model):
 
     name = fields.Char(string='Title', required=True)
     description = fields.Html(string='Description')
+    code = fields.Boolean(string='Has a code', help="the document has a code")
+    issue = fields.Boolean(
+        string='Controlled', help="the document has a issue number and issue date")
+    external_document = fields.Boolean(
+        string='External Document', help="the document is an external file")
+    template = fields.Many2one(
+        'knowledge.document.template', string='Preferred template', help="the preferred template in case it's internal document")
